4babeaa92172cfd2179404576995d3a20cfa3252
Slawomir Siudek <siudeks@gmail.com>
2018-05-21 23:50:10 +0200

    Add tests and README.md

--- kafka-java/README.md
+++ kafka-java/README.md
@@ -1,6 +1,8 @@
 # Kafka with Spring
 
-Inspired by [Spring Tips: Spring Cloud Stream Kafka Streams](https://youtu.be/YPDzcmqwCNo)
+Inspired by 
+* [Spring Tips: Spring Cloud Stream Kafka Streams](https://youtu.be/YPDzcmqwCNo)
+* [Spring Kafka - Embedded Unit Test Example](https://www.codenotfound.com/spring-kafka-embedded-unit-test-example.html)
 
-To run test and watch changes in source files:
-* gradlew build --continuous
+To run test:
+* gradlew build
--- kafka-java/src/test/java/com/example/demo/DemoApplicationTests.java
+++ kafka-java/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,23 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@ClassRule
+  	public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, "topic");
+
 	@Test
 	public void contextLoads() {
-
+		Assertions.assertThat(embeddedKafka).isNotNull();
 	}
 
 }
